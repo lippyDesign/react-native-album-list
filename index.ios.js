13a4d8f99b164f3bf46f2da2357af489be01da6e
Volodymyr Lipunov <volo_lipu@yahoo.com>
2016-09-24 12:30:22 -0700

    made several changes to how data is displayed

--- index.ios.js
+++ index.ios.js
@@ -2,13 +2,16 @@
 
 // Import a library to help create component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // import components
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 // Create component
 const App = () => (
-    
-    <Header headerText={'Albums'} />
+    <View style={{ flex: 1 }} >
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 
 // Render it to the device
